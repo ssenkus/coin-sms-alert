24b0e4d1ff152d681c064c267d78928fa4b30fbb
Steve Senkus <steve.senkus@gmail.com>
2018-05-10 20:31:39 -0700

    added comments

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+// Requests to http://localhost:3000/ will respond with a JSON object
 app.get('/', (req, res) => {
     res.json({success: true});
 });
 
+// Start listening on port 3000 to start receiving requests
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 });
